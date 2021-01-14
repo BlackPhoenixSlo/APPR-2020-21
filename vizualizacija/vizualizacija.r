5d2d87ff3222a220ec58b608f45c11ef38e97a50
BlackPhoenixSlo <49908413+BlackPhoenixSlo@users.noreply.github.com>
2021-01-14 08:51:42 +0100

    Zadnji popravki

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -10,9 +10,6 @@ levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
   { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
 zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
 
 # geom_col
 # geom_bar
