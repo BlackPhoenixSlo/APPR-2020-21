cdffc41ff86b136c5d691c1bcfd0f64e0967e089
BlackPhoenixSlo <49908413+BlackPhoenixSlo@users.noreply.github.com>
2021-01-27 15:32:50 +0100

    popravek

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,19 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+
+
+
+Sestevek_po_pridelkih_regijah$leto <- as.character(Sestevek_po_pridelkih_regijah$leto)
+Sestevek_po_pridelkih_regijah$Kolicina <- as.character(Sestevek_po_pridelkih_regijah$Kolicina)
+
+
+Sestevek_po_pridelkih_regijah <- Sestevek_po_pridelkih_regijah %>% mutate(leto = parse_integer(leto), Kolicina = parse_number(Kolicina))
+prileganje <- lm(Kolicina ~ leto, data = Sestevek_po_pridelkih_regijah)
+predict(prileganje, data.frame(leto=seq(2010,2022,1)))
+
+
+graf_napoved <- ggplot(Sestevek_po_pridelkih_regijah, aes(x=leto, y=Kolicina)) + geom_point() + 
+  geom_smooth(method='lm', formula=y ~ poly(x,2,raw=TRUE), fullrange=TRUE, color='green') + scale_x_continuous('leto', breaks = seq(2010, 2022, 1), limits = c(2010,2022))
+
+
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -101,19 +101,6 @@ graf_indeks_reduciran <- graf_indeks_reduciran + xlab('Leto proizvodnje') + ylab
 graf_indeks_reduciran <- ggplotly(graf_indeks_reduciran)
 
 #??=================
-Sestevek_po_pridelkih_regijah$leto <- as.character(Sestevek_po_pridelkih_regijah$leto)
-Sestevek_po_pridelkih_regijah$Kolicina <- as.character(Sestevek_po_pridelkih_regijah$Kolicina)
-
-
-Sestevek_po_pridelkih_regijah <- Sestevek_po_pridelkih_regijah %>% mutate(leto = parse_integer(leto), Kolicina = parse_number(Kolicina))
-prileganje <- lm(Kolicina ~ leto, data = Sestevek_po_pridelkih_regijah)
-predict(prileganje, data.frame(leto=seq(2010,2022,1)))
-
-
-graf_napoved <- ggplot(Sestevek_po_pridelkih_regijah, aes(x=leto, y=Kolicina)) + geom_point() + 
-  geom_smooth(method='lm', formula=y ~ poly(x,2,raw=TRUE), fullrange=TRUE, color='green') + scale_x_continuous('leto', breaks = seq(2010, 2022, 1), limits = c(2010,2022))
-
-
 
 
 
